3744062fc2944431b380d3e0d31fcc3e1a66996c
mlabib2 <mahirlive2000@gmail.com>
2025-03-07 22:34:02 +0800

    feat: AnimatedCounter, TotalbalanceBox, CountUp

--- constants/TotalBalanceBox.tsx
+++ constants/TotalBalanceBox.tsx
@@ -1,12 +1,34 @@
+'use client';
 import React from 'react'
-
+import AnimatedCounter from './AnimatedCounter'
 const TotalBalanceBox = ({
     accounts = [], totalBanks, totalCurrentBalance}:TotlaBalanceBoxProps) => {
   return (
-    <div>
+    <section className='total-balance'>
+        <div className='total-balance-chart'>
+            {/* Donut Chart  */}
+        </div>
+        <div className='flex flex-col gap-6'>
+            <h2 className='header-2'>
+                 Bank Accounts: {totalBanks}
+    
+            </h2> 
+            <div className='flex flex-col gap-2'>
+                <p className='total-balance-label'>
+                    Total Current Balance
+                </p>
+                <p className='total-balance-amount flex-center gap-2'>
+                    <AnimatedCounter amount = {totalCurrentBalance}>
+                        
+                    </AnimatedCounter>
+                </p>
+            </div>
 
-    </div>
+        </div>
+    </section>
   )
 }
 
 export default TotalBalanceBox
+
+
