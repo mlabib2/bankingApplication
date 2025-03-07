05d085d87648839b98cd269cc08110236e9601fc
mlabib2 <mahirlive2000@gmail.com>
2025-03-07 21:21:40 +0800

    feat: Welcome Text, HeaderBox in constants && root/layout && root/page.tsx

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,27 @@
+import HeaderBox from '@/constants/HeaderBox'
+import TotalBalanceBox from '@/constants/TotalBalanceBox';
 import React from 'react'
 
 const Page = () => {
+  const loggedIn = {firstName: "Mahir"};
   return (
-    <section classname="home">
-      <div classname="home-content"></div>
-      <header classname="home-header"></header>
+    <section className="home">
+      <div className="home-content">
+      <header className="home-header">
+        <HeaderBox
+          type = "greeting"
+          title = "welcome"
+          user = {loggedIn?.firstName || "Guest"}
+          subtext = "Access and manage your account and transactions efficiently"
+        >
+        </HeaderBox>
+        <TotalBalanceBox
+        accounts = {[]}
+        totalBanks = {1}
+        totalCurrentBalance = {1250.35}      
+        ></TotalBalanceBox>
+      </header>
+      </div>
     </section>
   )
 }
---
+++ constants/TotalBalanceBox.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const TotalBalanceBox = ({
+    accounts = [], totalBanks, totalCurrentBalance}:TotlaBalanceBoxProps) => {
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default TotalBalanceBox
