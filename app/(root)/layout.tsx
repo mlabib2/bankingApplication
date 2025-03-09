0eed0bc33a4740615548f754f71ca78f04210c0d
mlabib2 <mahirlive2000@gmail.com>
2025-03-10 00:56:48 +0800

    Moved uncommitted changes to third branch

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,23 +1,18 @@
-import { Main } from "@/node_modules/next/document";
-import { Children } from "react";
+import React from "react";
 import Sidebar from "@/constants/sidebar";
+
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   const LoggedIn = { firstName: "Mahir", lastName: "Labib" };
+
   return (
     <main className="flex h-screen w-full font-inter">
       <Sidebar user={LoggedIn} />
+      SIDEBAR
       {children}
-
-
     </main>
   );
 }
-
-
-
-
-
--- constants/Sidebar.tsx
+++ constants/Sidebar.tsx
@@ -1,14 +1,50 @@
-import React from 'react'
+'use client'
+import Link from "next/link";
+import React from "react";
+import Image from "next/image";
+import { ITEMS, sidebarLinks } from "./index"; // Ensure correct import
+import { cn } from "@/lib/utils";
+import { usePathname } from "@/node_modules/next/navigation";
 
-const Sidebar = ({user}: SiderbarProps) => {
-  return (
-    <section className='sidebar'> 
-        <nav className='flex flex-col gap-4'>
-
-
-    </nav>
-    </section>
-  )
+interface SidebarProps {
+  user?: {
+    firstName: string;
+    lastName: string;
+  };
 }
 
-export default Sidebar
+const Sidebar: React.FC<SidebarProps> = ({ user }) => {
+    const pathName = usePathname();
+  return (
+    <section className="sidebar p-4">
+      <nav className="flex flex-col gap-4">
+        {/* Logo Section */}
+        <Link href="/" className="mb-12 cursor-pointer flex items-center gap-2">
+          <Image
+            src="/icons/logo.svg"
+            width={34}
+            height={34}
+            alt="Horizontal logo"
+            className="size-[24px] max-xl:size-14"
+          />
+          <h1 className="sidebar-logo">Horizon</h1>
+        </Link>
+
+        {/* Sidebar Navigation Links */}
+        {sidebarLinks.map((item) => {
+    const isActive = pathName === item.route || pathName.startsWith(item.route);
+    return (
+      <Link href={item.route} key={item.label} className={cn('sidebar-link flex items-center gap-2 p-2 rounded-md', {
+        'bg-bank-gradient': isActive
+      })}>
+        <Image src={item.imgURL} width={24} height={24} alt={item.label} />
+        <span>{item.label}</span>
+      </Link>
+    );
+})}
+      </nav>
+    </section>
+  );
+};
+
+export default Sidebar;
