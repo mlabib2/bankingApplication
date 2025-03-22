bf52d4fd4c5b0052651e8efd6ee28fbffeabc1c7
mlabib2 <mahirlive2000@gmail.com>
2025-03-22 22:49:36 +0800

    refactor(root page.tsx) cleaner code

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,29 +1,29 @@
-import HeaderBox from '@/constants/HeaderBox'
-import TotalBalanceBox from '@/constants/TotalBalanceBox';
-import React from 'react'
+import HeaderBox from "@/constants/HeaderBox";
+import TotalBalanceBox from "@/constants/TotalBalanceBox";
+// import { Title } from 'chart.js';
+import React from "react";
 
 const Page = () => {
-  const loggedIn = {firstName: "Mahir"};
+  const loggedIn = { firstName: "Mahir" };
   return (
     <section className="home">
       <div className="home-content">
-      <header className="home-header">
-        <HeaderBox
-          type = "greeting"
-          title = "welcome"
-          user = {loggedIn?.firstName || "Guest"}
-          subtext = "Access and manage your account and transactions efficiently"
-        >
-        </HeaderBox>
-        <TotalBalanceBox
-        accounts = {[]}
-        totalBanks = {1}
-        totalCurrentBalance = {1250.35}
-        ></TotalBalanceBox>
-      </header>
+        <header className="home-header">
+          <HeaderBox
+            type="greeting"
+            title="welcome"
+            subtext="Access and manage your account and transactions efficiently"
+            user={loggedIn?.firstName || "Guest"}
+          />
+          <TotalBalanceBox
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={1250.35}
+          ></TotalBalanceBox>
+        </header>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default Page
+export default Page;
