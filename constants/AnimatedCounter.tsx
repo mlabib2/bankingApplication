863ef65be4d7e090da047f53bc72b860e3f4f66b
mlabib2 <mahirlive2000@gmail.com>
2025-03-22 22:29:27 +0800

    Push rest of annimatedCounter

--- constants/AnimatedCounter.tsx
+++ constants/AnimatedCounter.tsx
@@ -1,17 +1,12 @@
-import React from 'react'
-import CountUp from 'react-countup';
+import React from "react";
+import CountUp from "react-countup";
 
-const AnimatedCounter = ({amount}:{amount:number}) => {
+const AnimatedCounter = ({ amount }: { amount: number }) => {
   return (
-    <div className='w-full'>
-        <CountUp 
-        end = {amount}
-        decimals= {2}
-        prefix="$"
-        duration={2.75}
-        ></CountUp>
+    <div className="w-full">
+      <CountUp end={amount} decimals={2} prefix="$" duration={2.75}></CountUp>
     </div>
-  )
-}
+  );
+};
 
-export default AnimatedCounter
+export default AnimatedCounter;
