c7935a0337ede1176bb6a6625f22b9852147b0b9
mlabib2 <mahirlive2000@gmail.com>
2025-03-29 13:58:30 +0800

    feat (MobileNav) initiation

--- constants/MobileNav.tsx
+++ constants/MobileNav.tsx
@@ -1,7 +1,12 @@
 "use client";
 
 import React from "react";
-import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
+import {
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetTrigger,
+} from "@/components/ui/sheet";
 import Image from "next/image";
 import { cn } from "@/lib/utils";
 import Link from "next/link";
@@ -38,35 +43,42 @@ const MobileNav = () => {
             </h1>
           </Link>
 
-          {/* Sidebar Navigation Links */}
-          {sidebarLinks.map((item) => {
-            const isActive =
-              pathname === item.route || pathname.startsWith(item.route);
+          <div className="mobilenav-sheet">
+            {/* SheetClose --> Click elsewhere to close the sheet  */}
+            <SheetClose asChild>
+              <nav className="flex h-full flex-col gap-6 pt-16 text-white">
+                {/* Sidebar Navigation Links */}
+                {sidebarLinks.map((item) => {
+                  const isActive =
+                    pathname === item.route || pathname.startsWith(item.route);
+                  return (
+                    <SheetClose asChild key={item.route}>
+                      <Link
+                        href={item.route}
+                        key={item.label}
+                        className={cn("mobilenav-sheet_close w-full", {
+                          "bg-bank-gradient": isActive,
+                        })}
+                        style={{ color: isActive ? "#ffffff" : "#000000" }} // Explicit inline styling for color
+                      >
+                        <Image
+                          src={item.imgURL}
+                          alt={item.label}
+                          width={20}
+                          height={20}
+                          className={cn({
+                            "brightness-[3] invert-0": isActive,
+                          })}
+                        />
 
-            return (
-              <Link
-                href={item.route}
-                key={item.label}
-                className={cn(
-                  "sidebar-link flex items-center gap-2 p-2 rounded-md",
-                  {
-                    "bg-bank-gradient": isActive,
-                  }
-                )}
-                style={{ color: isActive ? "#ffffff" : "#000000" }} // Explicit inline styling for color
-              >
-                <Image
-                  src={item.imgURL}
-                  width={24}
-                  height={24}
-                  alt={item.label}
-                  className={cn({ "brightness-3 invert-0": isActive })}
-                />
-
-                <span>{item.label}</span>
-              </Link>
-            );
-          })}
+                        <span>{item.label}</span>
+                      </Link>
+                    </SheetClose>
+                  );
+                })}
+              </nav>
+            </SheetClose>
+          </div>
         </SheetContent>
       </Sheet>
     </section>
